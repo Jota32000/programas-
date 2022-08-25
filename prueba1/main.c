0f015a252f5651769da80f8d515a7d7a28c62382
absalón seguel <absalonseguel@gmail.com>
2022-08-25 14:36:26 -0400

    la vieja de los valores

--- prueba1/main.c
+++ prueba1/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    int min, max;
+    printf("ingrese min: ");
+    scanf("%i",&min);
+    printf("ingrese max: ");
+    scanf("%i",&max);
+
+    int l=(max-min)+1;
+    int arr[l];
+
+    for (int k = 0; k < l ; ++k)
+    {
+        arr[k]=k+min;
+        printf("%i,",arr[k]);
+    }
     return 0;
-}
+};
+
+
+
+
